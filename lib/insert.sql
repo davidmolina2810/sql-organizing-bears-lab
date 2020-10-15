c34356778128ed38d107e1dc6f3989320cd85c2c
davidmolina2810 <davidmolina2810@gmail.com>
2020-10-15 13:28:24 -0500

    Finished lab.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 2, "M", NULL, NULL, NULL);
+INSERT INTO bears VALUES (2, "Rowdy", 4, "F", NULL, NULL, NULL);
+INSERT INTO bears VALUES (3, "Tabitha", 10, "M", NULL, NULL, NULL);
+INSERT INTO bears VALUES (4, "Sergent Brown", 5, "F", NULL, NULL, NULL);
+INSERT INTO bears VALUES (5, "Melissa", 8, "M", NULL, NULL, NULL);
+INSERT INTO bears VALUES (6, "Grinch", 12, "F", NULL, NULL, NULL);
+INSERT INTO bears VALUES (7, "Wendy", 1, "M", NULL, NULL, NULL);
+INSERT INTO bears VALUES (8, NULL, 2, "F", NULL, NULL, NULL);
